44e12879eb6a1d5a434278d9dab51916edf77a52
Stufingo <geniekort@hotmail.com>
2016-01-01 15:09:21 +0100

    First Commit

---
+++ BarF/src/Test.java
@@ -0,0 +1,14 @@
+/**
+ * @author s141705
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public void print(){
+		
+		System.out.println("Doei");
+	}
+}
---
+++ BarF/src/subTest.java
@@ -0,0 +1,9 @@
+
+public class subTest extends Test{
+
+	
+	public static void main(String[] args) {
+		subTest x = new subTest();
+		x.print();
+	}
+}
